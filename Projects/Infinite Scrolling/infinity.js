3e431b4ed7935cb24d7ab6f70b27573d0c36a016
anandwetzel <anandwetzel@gmail.com>
2023-01-30 15:55:11 -0500

    infinite scrolling

---
+++ Projects/Infinite Scrolling/infinity.js
@@ -0,0 +1,87 @@
+const DEFAULT_PAGE_SIZE = 5;
+const DEFAULT_SORT_ORDER = 'recent';
+
+const States = {
+  PENDING: 'pending',
+  READY: 'ready',
+  BACKOFF: 'backoff'
+};
+
+let componentState = States.READY;
+
+function isComponentPending() {
+  return componentState === States.PENDING;
+}
+
+function setPending() {
+  componentState = States.PENDING;
+  document.body.appendChild(loadingElement);
+}
+
+function setReady() {
+  componentState = States.READY;
+  document.body.removeChild(loadingElement);
+}
+
+function setBackoff() {
+  componentState = States.BACKOFF;
+  document.body.removeChild(loadingElement);
+}
+
+let lastTweetId = null;
+
+const loadingElement = document.createElement('div');
+// Give it the same style
+loadingElement.classList.add('tweet');
+loadingElement.innerHTML = `
+  ...Loading...
+`;
+
+function onNewTweets(data) {
+  if (data.length <= 1) {
+      setBackoff();
+      setTimeout(() => setReady(), 2000);
+  } else {
+  setReady();
+  let tweetsHTML = '';
+  for (const tweetResponse of data) {
+    const tweet = createTweet(tweetResponse.tweet);
+    tweetsHTML += tweet;
+    lastTweetId = tweetResponse.id;
+  }
+  document.body.innerHTML += tweetsHTML;
+  }
+}
+
+function hydrate() {
+  const params = {
+    pageSize: DEFAULT_PAGE_SIZE,
+    sortOrder: DEFAULT_SORT_ORDER
+  }
+  api.get(HOST + 'tweets', params, onNewTweets);
+  setPending();
+}
+
+loadTestData();
+hydrate();
+
+function onScroll(event) {
+  if (isComponentPending()) {
+    return;
+  }
+  const scrolledTo = window.innerHeight + window.pageYOffset;
+  const scrollLimit = document.body.offsetHeight;
+  const scrollThreshold = 30;
+
+  if (scrollLimit - scrolledTo <= scrollThreshold && componentState != 'backoff') {
+    const params = {
+      pageSize: DEFAULT_PAGE_SIZE,
+      sortOrder: DEFAULT_SORT_ORDER,
+      lastTweetId
+    }
+    api.get(HOST + 'tweets', params, onNewTweets);
+    setPending();
+  }
+}
+
+window.addEventListener('scroll', onScroll);
